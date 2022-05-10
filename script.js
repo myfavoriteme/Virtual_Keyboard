9ef9e8472791cc13d4ab8f9bf203733b208554ed
Vladislav Kakorin <vlad.kakorin03@gmail.com>
2022-05-11 00:13:20 +0300

    feat: add more keys

--- script.js
+++ script.js
@@ -1,8 +1,465 @@
+let keysEng = [
+    {
+        "class": "backquote",
+        "text": "`",
+        "upperCase": "~",
+        "text_ru": "ё",
+        "upperCase_ru": "Ё"
+    },
+    {
+        "class":"digit_1",
+        "text": "1",
+        "upperCase": "!",
+        "text_ru": "1",
+        "upperCase_ru": "!"
+    },
+    {
+        "class":"digit_2",
+        "text": "2",
+        "upperCase": "@",
+        "text_ru": "2",
+        "upperCase_ru": '"'
+    },
+    {
+        "class":"digit_3",
+        "text": "3",
+        "upperCase": "#",
+        "text_ru": "3",
+        "upperCase_ru": "№"
+    },
+    {
+        "class":"digit_4",
+        "text": "4",
+        "upperCase": "$",
+        "text_ru": "4",
+        "upperCase_ru": ";"
+    },
+    {
+        "class":"digit_5",
+        "text": "5",
+        "upperCase": "%",
+        "text_ru": "5",
+        "upperCase_ru": "%"
+    },
+    {
+        "class":"digit_6",
+        "text": "6",
+        "upperCase": "^",
+        "text_ru": "6",
+        "upperCase_ru": ":"
+    },
+    {
+        "class":"digit_7",
+        "text": "7",
+        "upperCase": "&",
+        "text_ru": "7",
+        "upperCase_ru": "?"
+    },
+    {
+        "class":"digit_8",
+        "text": "8",
+        "upperCase": "*",
+        "text_ru": "8",
+        "upperCase_ru": "*"
+    },
+    {
+        "class":"digit_9",
+        "text": "9",
+        "upperCase": "(",
+        "text_ru": "9",
+        "upperCase_ru": "("
+    },
+    {
+        "class":"digit_0",
+        "text": "0",
+        "upperCase": ")",
+        "text_ru": "0",
+        "upperCase_ru": ")"
+    },
+    {
+        "class":"minus",
+        "text":"-",
+        "upperCase": "_",
+        "text_ru": "-",
+        "upperCase_ru": "_"
+    },
+    {
+        "class":"equal",
+        "text":"=",
+        "upperCase": "+",
+        "text_ru": "=",
+        "upperCase_ru": "+"
+    },
+    {
+        "class":"backspace",
+        "text":"Backspace",
+        "upperCase": "Backspace",
+        "text_ru": "Backspace",
+        "upperCase_ru": "Backspace"
+    },
+    {
+        "class":"tab",
+        "text":"Tab",
+        "upperCase": "Tab",
+        "text_ru": "Tab",
+        "upperCase_ru": "Tab"
+    },
+    {
+        "class":"KeyQ",
+        "text":"q",
+        "upperCase": "Q",
+        "text_ru": "й",
+        "upperCase_ru": "Й"
+    },
+    {
+        "class":"KeyW",
+        "text":"w",
+        "upperCase": "W",
+        "text_ru": "ц",
+        "upperCase_ru": "Ц"
+    },
+    {
+        "class":"KeyE",
+        "text":"e",
+        "upperCase": "E",
+        "text_ru": "у",
+        "upperCase_ru": "У"
+    },
+    {
+        "class":"keyR",
+        "text":"r",
+        "upperCase": "R",
+        "text_ru": "к",
+        "upperCase_ru": "К"
+    },
+    {
+        "class":"keyT",
+        "text":"t",
+        "upperCase": "T",
+        "text_ru": "е",
+        "upperCase_ru": "Е"
+    },
+    {
+        "class":"KeyY",
+        "text":"y",
+        "upperCase": "Y",
+        "text_ru": "н",
+        "upperCase_ru": "Н"
+    },
+    {
+        "class":"KeyU",
+        "text":"u",
+        "upperCase": "U",
+        "text_ru": "г",
+        "upperCase_ru": "Г"
+    },
+    {
+        "class":"KeyI",
+        "text":"i",
+        "upperCase": "I",
+        "text_ru": "ш",
+        "upperCase_ru": "Ш"
+    },
+    {
+        "class":"KeyO",
+        "text":"o",
+        "upperCase": "O",
+        "text_ru": "щ",
+        "upperCase_ru": "Щ"
+    },
+    {
+        "class":"KeyP",
+        "text":"p",
+        "upperCase": "P",
+        "text_ru": "з",
+        "upperCase_ru": "З"
+    },
+    {
+        "class":"bracket_left",
+        "text":"[",
+        "upperCase": "{",
+        "text_ru": "х",
+        "upperCase_ru": "Х"
+    },
+    {
+        "class":"bracket_right",
+        "text":"]",
+        "upperCase": "}",
+        "text_ru": "ъ",
+        "upperCase_ru": "Ъ"
+    },
+    {
+        "class":"BackSlash",
+        "text": "&frasl;",
+        "upperCase": "|",
+        "text_ru": "&frasl;",
+        "upperCase_ru": "|"
+    },
+    {
+        "class":"delete",
+        "text":"Del",
+        "upperCase": "Del",
+        "text_ru": "Del",
+        "upperCase_ru": "Del"
+    },
+    {
+        "class":"CapsLock",
+        "text":"CapsLock",
+        "upperCase": "CapsLock",
+        "text_ru": "CapsLock",
+        "upperCase_ru": "CapsLock",
+    },
+    {
+        "class":"KeyA",
+        "text":"a",
+        "upperCase": "A",
+        "text_ru": "ф",
+        "upperCase_ru": "Ф",
+    },
+    {
+        "class":"KeyS",
+        "text":"s",
+        "upperCase": "S",
+        "text_ru": "ы",
+        "upperCase_ru": "Ы",
+    },
+    {
+        "class":"KeyD",
+        "text":"d",
+        "upperCase": "D",
+        "text_ru": "",
+        "upperCase_ru": "",
+    },
+    {
+        "class":"KeyF",
+        "text":"f",
+        "upperCase": "F",
+        "text_ru": "а",
+        "upperCase_ru": "А",
+    },
+    {
+        "class":"KeyG",
+        "text":"g",
+        "upperCase": "G",
+        "text_ru": "п",
+        "upperCase_ru": "П",
+    },
+    {
+        "class":"KeyH",
+        "text":"h",
+        "upperCase": "H",
+        "text_ru": "р",
+        "upperCase_ru": "Р",
+    },
+    {
+        "class":"KeyJ",
+        "text":"j",
+        "upperCase": "J",
+        "text_ru": "о",
+        "upperCase_ru": "О",
+    },
+    {
+        "class":"KeyK",
+        "text":"k",
+        "upperCase": "K",
+        "text_ru": "л",
+        "upperCase_ru": "Л",
+    },
+    {
+        "class":"KeyL",
+        "text":"l",
+        "upperCase": "L",
+        "text_ru": "д",
+        "upperCase_ru": "Д",
+    },
+    {
+        "class":"semicolon",
+        "text":";",
+        "upperCase": ":",
+        "text_ru": "ж",
+        "upperCase_ru": "Ж",
+    },
+    {
+        "class":"quote",
+        "text":"'",
+        "upperCase": '"',
+        "text_ru": "э",
+        "upperCase_ru": "Э",
+    },
+    {
+        "class":"Enter",
+        "text":"Enter",
+        "upperCase": "Enter",
+        "text_ru": "Enter",
+        "upperCase_ru": "Enter",
+    },
+    {
+        "class":"Shift",
+        "text":"Shift",
+        "upperCase": "Shift",
+        "text_ru": "Shift",
+        "upperCase_ru": "Shift",
+    },
+    {
+        "class":"KeyZ",
+        "text":"z",
+        "upperCase": "Z",
+        "text_ru": "",
+        "upperCase_ru": "",
+    },
+    {
+        "class":"KeyX",
+        "text":"x",
+        "upperCase": "x",
+        "text_ru": "",
+        "upperCase_ru": "",
+    },
+    {
+        "class":"KeyC",
+        "text":"c",
+        "upperCase": "C",
+        "text_ru": "",
+        "upperCase_ru": "",
+    },
+    {
+        "class":"KeyV",
+        "text":"v",
+        "upperCase": "V",
+        "text_ru": "",
+        "upperCase_ru": "",
+    },
+    {
+        "class":"KeyB",
+        "text":"b",
+        "upperCase": "b",
+        "text_ru": "",
+        "upperCase_ru": "",
+    },
+    {
+        "class":"KeyN",
+        "text":"n",
+        "upperCase": "N",
+        "text_ru": "",
+        "upperCase_ru": "",
+    },
+    {
+        "class":"KeyM",
+        "text":"m",
+        "upperCase": "M",
+        "text_ru": "",
+        "upperCase_ru": "",
+    },
+    {
+        "class":"Comma",
+        "text":",",
+        "upperCase": "<",
+        "text_ru": "",
+        "upperCase_ru": "",
+    },
+    {
+        "class":"Dot",
+        "text":".",
+        "upperCase": ">",
+        "text_ru": "",
+        "upperCase_ru": "",
+    },
+    {
+        "class":"Slash",
+        "text":"/",
+        "upperCase": "?",
+        "text_ru": "",
+        "upperCase_ru": "",
+    },
+    {
+        "class":"Up",
+        "text":"&#11205;",
+        "upperCase": "&uArr;",
+        "text_ru": "&uArr;",
+        "upperCase_ru": "&uArr;",
+    },
+    {
+        "class":"Shift",
+        "text":"Shift",
+        "upperCase": "Shift",
+        "text_ru": "Shift",
+        "upperCase_ru": "Shift",
+    },
+    {
+        "class":"Ctrl",
+        "text":"Ctrl",
+        "upperCase": "Ctrl",
+        "text_ru": "Ctrl",
+        "upperCase_ru": "Ctrl",
+    },
+    {
+        "class":"Win",
+        "text":"Win",
+        "upperCase": "Win",
+        "text_ru": "Win",
+        "upperCase_ru": "Win",
+    },
+    {
+        "class":"Alt",
+        "text":"Alt",
+        "upperCase": "Alt",
+        "text_ru": "Alt",
+        "upperCase_ru": "Alt",
+    },
+    {
+        "class":"Space",
+        "text":"&#32;",
+        "upperCase": "&#32;",
+        "text_ru": "&#32;",
+        "upperCase_ru": "&#32;",
+    },
+    {
+        "class":"Alt",
+        "text":"Alt",
+        "upperCase": "Alt",
+        "text_ru": "Alt",
+        "upperCase_ru": "Alt",
+    },
+    {
+        "class":"left",
+        "text":"&#11207;",
+        "upperCase": "&#11207;",
+        "text_ru": "&#11207;",
+        "upperCase_ru": "&#11207;",
+    },
+    {
+        "class":"down",
+        "text":"&#11206;",
+        "upperCase": "&#11206;",
+        "text_ru": "&#11206;",
+        "upperCase_ru": "&#11206;",
+    },
+    {
+        "class":"right",
+        "text":"&#11208;",
+        "upperCase": "&#11208;",
+        "text_ru": "&#11208;",
+        "upperCase_ru": "&#11208;",
+    },
+    {
+        "class":"Ctrl",
+        "text":"Ctrl",
+        "upperCase": "Ctrl",
+        "text_ru": "Ctrl",
+        "upperCase_ru": "Ctrl",
+    },
+]
+
 let body = document.querySelector('body'),
     wrapper = document.createElement('div'),
     title = document.createElement('div'),
     text = document.createElement('textarea'),
-    keybord = document.createElement('div');
+    keyboard = document.createElement('div'),
+    line1 = document.createElement('div'),
+    line2 = document.createElement('div'),
+    line3 = document.createElement('div'),
+    line4 = document.createElement('div'),
+    line5 = document.createElement('div');
+
 
 function createElements (name, style, parrent) {
     parrent.append(name);
@@ -12,6 +469,68 @@ function createElements (name, style, parrent) {
 createElements(wrapper, 'wrapper', body);
 createElements(title, 'title', wrapper);
 createElements(text, 'textarea', wrapper);
-createElements(keybord, 'keybord', wrapper);
+createElements(keyboard, 'keyboard', wrapper);
+createElements(line1, 'line', keyboard);
+createElements(line2, 'line', keyboard);
+createElements(line3, 'line', keyboard);
+createElements(line4, 'line', keyboard);
+createElements(line5, 'line', keyboard);
 createElements(wrapper, 'wrapper', body);
-createElements(wrapper, 'wrapper', body);
+
+
+
+
+title.innerHTML = 'RSS Виртуальная клавиатура';
+
+//
+// Craeting keys
+//
+let style = '';
+
+function CreateLine(start, end, lineNumber) {
+    for (let i = start; i < end; i++) {
+        n = document.createElement('div');
+
+        spanEng = document.createElement('span');
+        spanRus = document.createElement('span');
+        textEng = document.createElement('span');
+        textEngUp = document.createElement('span');
+        textRus = document.createElement('span');
+        textRusUp = document.createElement('span');
+
+        lineNumber.append(n);
+        n.append(spanEng);
+        n.append(spanRus);
+        spanEng.append(textEng);
+        spanEng.append(textEngUp);
+        spanRus.append(textRus);
+        spanRus.append(textRusUp);
+
+        spanEng.classList.add('eng');
+        spanRus.classList.add('rus');
+
+        console.log(keysEng[i].class);
+        style = keysEng[i].class;
+        n.classList.add('key');
+        n.classList.add(style);
+        textEng.classList.add('eng__text');
+        textEngUp.classList.add('eng__text_up');
+        textEngUp.classList.add('hiden');
+        textRus.classList.add('rus__text');
+        textRus.classList.add('hiden');
+        textRusUp.classList.add('rus__text_up');
+        textRusUp.classList.add('hiden');
+        
+        textEng.innerHTML = keysEng[i].text;
+        textEngUp.innerHTML = keysEng[i].upperCase;
+        textRus.innerHTML = keysEng[i].text_ru;
+        textRusUp.innerHTML = keysEng[i].upperCase_ru;
+    }
+}
+
+CreateLine(0, 14, line1);
+CreateLine(14, 29, line2);
+CreateLine(29, 42, line3);
+CreateLine(42, 55, line4);
+CreateLine(55, 64, line5);
+
